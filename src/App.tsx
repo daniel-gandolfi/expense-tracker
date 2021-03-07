81dadde1f443c299cb3a904c655798fab954c293
Daniel Gandolfi <gandan97@hotmail.it>
2021-03-07 22:38:05 +0100

    add swipe functionality to swipe throgh data

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ import { usePromiseSafe } from 'ui/hooks/usePromiseSafe';
 import { transactionService } from 'services/transaction/PouchOrmTransactionService';
 import { ownerDao } from 'services/owner/OwnerService';
 import { categoryDao } from 'services/category/CategoryService';
+import Swipe from 'react-easy-swipe';
 
 function App() {
-  const todayDate = new Date();
+  const [dateToShow, changeDateToShow] = useState<Date>(new Date());
   const [isDbInit, setDbInit] = useState<boolean>(false);
   const [importing, setImport] = useState<boolean>(false);
   const [importError, setImportError] = useState<string>('');
@@ -19,6 +20,19 @@ function App() {
       setDbInit(true);
     }
   );
+
+  function onSwipeRight() {
+    const newMonth = dateToShow.getMonth() !== 0 ? dateToShow.getMonth() - 1 : 11;
+    const newYear = newMonth !== 11 ? dateToShow.getFullYear() : dateToShow.getFullYear() - 1;
+    changeDateToShow(new Date(newYear, newMonth));
+  }
+
+  function onSwipeLeft() {
+    const newMonth = dateToShow.getMonth() !== 11 ? dateToShow.getMonth() + 1 : 0;
+    const newYear = newMonth !== 0 ? dateToShow.getFullYear() : dateToShow.getFullYear() + 1;
+    changeDateToShow(new Date(newYear, newMonth));
+  }
+
   const importDataCallback = () => {
     setImport(true);
     importData(dollarbirdCsvStr).then(
@@ -27,7 +41,7 @@ function App() {
       },
       function (err) {
         setImport(false);
-        setImportError(err.toString)
+        setImportError(err.toString);
       }
     );
   };
@@ -38,8 +52,17 @@ function App() {
       return (
         <>
           <button onClick={importDataCallback}>Import data</button>
-          <Header month={todayDate.getMonth() - 1} year={todayDate.getFullYear()} />
-          <MonthPreview month={todayDate.getMonth() - 1} year={todayDate.getFullYear()} />;
+          <Header month={dateToShow.getMonth()} year={dateToShow.getFullYear()} />
+          <Swipe
+            allowMouseEvents={true}
+            tolerance={60}
+            onSwipeLeft={onSwipeLeft}
+            onSwipeRight={onSwipeRight}
+            innerRef={(x) => {
+              x;
+            }}>
+            <MonthPreview month={dateToShow.getMonth()} year={dateToShow.getFullYear()} />;
+          </Swipe>
         </>
       );
     }
