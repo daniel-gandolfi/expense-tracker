60f666d7c0ebcc4978e21a1deda0e87acae93836
Daniel Gandolfi <gandan97@hotmail.it>
2021-03-08 00:14:21 +0100

    WIP working on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import { importData } from 'services/DollarbirdCsvImporter';
 import { dollarbirdCsvStr } from 'dollarbird';
-import { MonthPreview } from 'ui/components/MonthPreview/MonthPreview';
 import { Header } from 'ui/components/Header/Header';
 import { usePromiseSafe } from 'ui/hooks/usePromiseSafe';
 import { transactionService } from 'services/transaction/PouchOrmTransactionService';
 import { ownerDao } from 'services/owner/OwnerService';
 import { categoryDao } from 'services/category/CategoryService';
-import Swipe from 'react-easy-swipe';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,Redirect
+} from "react-router-dom";
+import {MonthBalance} from "ui/components/MonthBalance";
 
 function App() {
-  const [dateToShow, changeDateToShow] = useState<Date>(new Date());
+  const [dateToShow, ] = useState<Date>(new Date());
   const [isDbInit, setDbInit] = useState<boolean>(false);
   const [importing, setImport] = useState<boolean>(false);
   const [importError, setImportError] = useState<string>('');
@@ -20,18 +24,6 @@ function App() {
       setDbInit(true);
     }
   );
-
-  function onSwipeRight() {
-    const newMonth = dateToShow.getMonth() !== 0 ? dateToShow.getMonth() - 1 : 11;
-    const newYear = newMonth !== 11 ? dateToShow.getFullYear() : dateToShow.getFullYear() - 1;
-    changeDateToShow(new Date(newYear, newMonth));
-  }
-
-  function onSwipeLeft() {
-    const newMonth = dateToShow.getMonth() !== 11 ? dateToShow.getMonth() + 1 : 0;
-    const newYear = newMonth !== 0 ? dateToShow.getFullYear() : dateToShow.getFullYear() + 1;
-    changeDateToShow(new Date(newYear, newMonth));
-  }
 
   const importDataCallback = () => {
     setImport(true);
@@ -51,18 +43,19 @@ function App() {
     } else {
       return (
         <>
-          <button onClick={importDataCallback}>Import data</button>
-          <Header month={dateToShow.getMonth()} year={dateToShow.getFullYear()} />
-          <Swipe
-            allowMouseEvents={true}
-            tolerance={60}
-            onSwipeLeft={onSwipeLeft}
-            onSwipeRight={onSwipeRight}
-            innerRef={(x) => {
-              x;
-            }}>
-            <MonthPreview month={dateToShow.getMonth()} year={dateToShow.getFullYear()} />;
-          </Swipe>
+          <Router>
+            <button onClick={importDataCallback}>Import data</button>
+            <Header month={dateToShow.getMonth()} year={dateToShow.getFullYear()} />
+
+            <Switch>
+              <Route path="/balance/byMonth/:year/:month" >
+                <MonthBalance/>
+              </Route>
+              <Route path={"/"}>
+                <Redirect to={`/balance/byMonth/${new Date().getFullYear()}/${new Date().getMonth()}`}/>
+              </Route>
+            </Switch>
+          </Router>
         </>
       );
     }
---
+++ src/ui/components/MonthBalance.tsx
@@ -0,0 +1,35 @@
+import Swipe from 'react-easy-swipe';
+import { MonthPreview } from 'ui/components/MonthPreview/MonthPreview';
+import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+export function MonthBalance() {
+  const history = useHistory();
+  const { month, year } = useParams<{month:string, year:string}>();
+  const yearNumber = year ? +year : new Date().getFullYear();
+  const monthNumber= month ? +month : new Date().getMonth();
+
+  function onSwipeRight() {
+    const newMonth = monthNumber !== 0 ? monthNumber - 1 : 11;
+    const newYear = newMonth !== 11 ? yearNumber : yearNumber - 1;
+    history.push(`/balance/byMonth/${newYear}/${newMonth}`);
+  }
+
+  function onSwipeLeft() {
+    const newMonth = monthNumber !== 11 ? monthNumber + 1 : 0;
+    const newYear = newMonth !== 0 ? yearNumber : yearNumber + 1;
+    history.push(`/balance/byMonth/${newYear}/${newMonth}`);
+  }
+  return (
+    <Swipe
+      allowMouseEvents={true}
+      tolerance={60}
+      onSwipeLeft={onSwipeLeft}
+      onSwipeRight={onSwipeRight}
+      innerRef={(x) => {
+        x;
+      }}>
+      <MonthPreview month={monthNumber} year={yearNumber} />
+    </Swipe>
+  );
+}
