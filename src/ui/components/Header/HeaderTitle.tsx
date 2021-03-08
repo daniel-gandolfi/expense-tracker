12d24114e926eb6c27b9a26e4ec0e5486b594280
Daniel Gandolfi <gandan97@hotmail.it>
2021-03-08 16:30:11 +0100

    handle day route in title

--- src/ui/components/Header/HeaderTitle.tsx
+++ src/ui/components/Header/HeaderTitle.tsx
@@ -1,12 +1,20 @@
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import { IMPORT_ROUTE, MONTH_BALANCE_ROUTE } from 'ui/utils/routes';
+import { DAY_BALANCE_ROUTE, IMPORT_ROUTE, MONTH_BALANCE_ROUTE } from 'ui/utils/routes';
 import React, { memo, useState } from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
 import { transactionService } from 'services/transaction/PouchOrmTransactionService';
 import { usePromiseSafe } from 'ui/hooks/usePromiseSafe';
 import { formatMoneyLocal } from 'ui/utils/formatting';
 
-function formatDate(date: Date) {
+function formatDayDate(date: Date) {
+  return new Intl.DateTimeFormat(navigator.language || 'it_IT', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }).format(date);
+}
+
+function formatMonthDate(date: Date) {
   return new Intl.DateTimeFormat(navigator.language || 'it_IT', {
     month: 'long',
     year: 'numeric'
@@ -18,13 +26,34 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1
   }
 }));
+function DailyBalanceTitle() {
+  const classes = useStyles();
+  const routeMatch = useRouteMatch<{ monthParam: string; yearParam: string; dayParam: string }>(
+    DAY_BALANCE_ROUTE
+  );
+  const year = routeMatch ? +routeMatch.params.yearParam : new Date().getFullYear();
+  const month = routeMatch ? +routeMatch.params.monthParam : new Date().getMonth();
+  const day = routeMatch ? +routeMatch.params.dayParam : new Date().getDate();
+  const title = formatDayDate(new Date(year, month, day));
+  const [dayBalance, setDayBalance] = useState<number>(0);
+
+  usePromiseSafe(transactionService.getBalanceForDay(year, month, day), setDayBalance, (x) =>
+    // eslint-disable-next-line no-console
+    console.error(x)
+  );
+  return (
+    <Typography variant="h6" className={classes.title}>
+      {title} ({formatMoneyLocal(dayBalance / 100)})
+    </Typography>
+  );
+}
 
 function MonthlyBalanceTitle() {
   const classes = useStyles();
   const routeMatch = useRouteMatch<{ monthParam: string; yearParam: string }>(MONTH_BALANCE_ROUTE);
   const year = routeMatch ? +routeMatch.params.yearParam : new Date().getFullYear();
   const month = routeMatch ? +routeMatch.params.monthParam : new Date().getMonth();
-  const title = formatDate(routeMatch ? new Date(year, month) : new Date());
+  const title = formatMonthDate(routeMatch ? new Date(year, month) : new Date());
   const [monthBalance, setMonthBalance] = useState<number>(0);
 
   usePromiseSafe(transactionService.getBalanceForMonth(year, month), setMonthBalance, (x) =>
@@ -50,6 +79,9 @@ const ImportTitle = memo(function _ImportTitle() {
 export function HeaderTitle() {
   return (
     <Switch>
+      <Route path={DAY_BALANCE_ROUTE}>
+        <DailyBalanceTitle />
+      </Route>
       <Route path={MONTH_BALANCE_ROUTE}>
         <MonthlyBalanceTitle />
       </Route>
