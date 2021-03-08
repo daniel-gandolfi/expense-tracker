55d398fc138accbbb5f6db7a6ecd1ef0354644f0
Daniel Gandolfi <gandan97@hotmail.it>
2021-03-08 16:29:50 +0100

    add day route

--- src/ui/utils/routes.ts
+++ src/ui/utils/routes.ts
@@ -5,5 +5,11 @@ export function createMonthBalanceRoute(year: number, month: number) {
     month.toString()
   );
 }
+export const DAY_BALANCE_ROUTE = '/balance/:yearParam/:monthParam/:dayParam';
+export function createDayBalanceRoute(year: number, month: number, day: number) {
+  return MONTH_BALANCE_ROUTE.replace(':yearParam', year.toString())
+    .replace(':monthParam', month.toString())
+    .replace(':dayParam', day.toString());
+}
 
 export const IMPORT_ROUTE = '/import';
