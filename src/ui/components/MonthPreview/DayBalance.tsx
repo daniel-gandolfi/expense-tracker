a41f7d69444e4bbf800f3e9462c0c722abd0fdab
Daniel Gandolfi <gandan97@hotmail.it>
2021-03-08 16:09:56 +0100

    reduce day balance font on small screen

--- src/ui/components/MonthPreview/DayBalance.tsx
+++ src/ui/components/MonthPreview/DayBalance.tsx
@@ -1,8 +1,22 @@
-import { Typography } from '@material-ui/core';
+import { Typography, useMediaQuery } from '@material-ui/core';
 import { formatMoneyLocal } from 'ui/utils/formatting';
 import React from 'react';
 
 type DayBalanceProps = { amount: number };
+
+function getFontSizeByMoneyFormatted(moneyFormatted: string) {
+  return '10px';
+}
+
 export function DayBalance({ amount }: DayBalanceProps) {
-  return <Typography variant={'body2'}>{formatMoneyLocal(amount / 100)}</Typography>;
+  const isSmall = useMediaQuery('sm');
+  const moneyFormatted = formatMoneyLocal(amount / 100);
+
+  return (
+    <Typography
+      variant={isSmall ? 'caption' : 'body2'}
+      style={{ fontSize: getFontSizeByMoneyFormatted(moneyFormatted) }}>
+      {moneyFormatted}
+    </Typography>
+  );
 }
